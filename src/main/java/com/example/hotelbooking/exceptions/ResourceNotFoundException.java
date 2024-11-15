f9bdc9a4058dfd969047926913186e6c9ae93afe
Saltanat Umarova <umarovasaltanat51@gmail.com>
2024-11-15 09:04:14 +0600

    Exceprions added

--- src/main/java/com/example/hotelbooking/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/example/hotelbooking/exceptions/ResourceNotFoundException.java
@@ -1,10 +1,14 @@
 package com.example.hotelbooking.exceptions;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
+@Getter
 public class ResourceNotFoundException extends RuntimeException {
+
     public ResourceNotFoundException(String message) {
         super(message);
     }
--- src/main/java/com/example/hotelbooking/exceptions/RoomNotAvailableException.java
+++ src/main/java/com/example/hotelbooking/exceptions/RoomNotAvailableException.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
 public class RoomNotAvailableException extends RuntimeException {
+
     public RoomNotAvailableException(String message) {
         super(message);
     }
